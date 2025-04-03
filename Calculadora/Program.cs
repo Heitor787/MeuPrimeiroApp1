ad2425c85affde771337afc56884b21a007d5f4e
Heitor787 <gabrielheitorhg@gmail.com>
2025-04-03 10:49:37 -0300

    Atividades

---
+++ Calculadora/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // digite dois números
+        Console.WriteLine("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Exiba o menu de opções
+        Console.WriteLine("Escolha a operação:");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        // Ler o que o usuário escolheu
+        int escolha = Convert.ToInt32(Console.ReadLine());
+
+        // Realizar a operação com switch
+        switch (escolha)
+        {
+            case 1:
+                Console.WriteLine("Resultado: " + (num1 + num2));
+                break;
+            case 2:
+                Console.WriteLine("Resultado: " + (num1 - num2));
+                break;
+            case 3:
+                Console.WriteLine("Resultado: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0)
+                    Console.WriteLine("Resultado: " + (num1 / num2));
+                else
+                    Console.WriteLine("Erro: Não é possível dividir por zero.");
+                break;
+            default:
+                Console.WriteLine("Opção inválida. Tente novamente.");
+                break;
+        }
+    }
+}
---
+++ Conceitos/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite a nota do aluno: ");
+        double nota = Convert.ToDouble(Console.ReadLine());
+
+        if (nota >= 9)
+            Console.WriteLine("Conceito: A");
+        else if (nota >= 7)
+            Console.WriteLine("Conceito: B");
+        else if (nota >= 5)
+            Console.WriteLine("Conceito: C");
+        else
+            Console.WriteLine("Conceito: D");
+    }
+}
