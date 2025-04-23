56e16faedb5140e56221ec7c6bfe64c45e28b5c2
Heitor787 <gabrielheitorhg@gmail.com>
2025-04-23 09:48:46 -0300

    C# bugadasso

---
+++ Exército/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int idade;
+        float altura;
+
+        Console.WriteLine("Informe a sua idade");
+        idade = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe a sua altura");
+        altura = float.Parse(Console.ReadLine());
+
+        if (altura >= 1.65 && idade >= 18)
+        {
+            Console.WriteLine("Você pode se alistar");
+        }
+        else
+        {
+            Console.Write("Você não pode se alistar");
+        }
+        
+    }
+}
